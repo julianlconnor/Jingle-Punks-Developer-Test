76134c99f5075f786215b7ddbc59cbe9032e5e1d
Julian Connor <connoj3@rpi.edu>
2011-05-01 21:36:50 -0400

    Fixed Data Migration

--- rails/db/migrate/20110430171901_create_role_users.rb
+++ rails/db/migrate/20110430171901_create_role_users.rb
@@ -4,6 +4,16 @@ class CreateRoleUsers < ActiveRecord::Migration
       t.references :role
       t.references :user
     end
+    User.reset_column_information
+    Role.reset_column_information
+    User.all.each do |user|
+      # Some users do not have roles, only build entries for users with roles
+      if !user.role_id.nil?
+        @role = Role.first(:conditions => {:id => user.role_id})
+        roles_users_object = RoleUser.new(:role_id => @role.id, :user_id => user.id)
+        roles_users_object.save!
+      end
+    end
   end
 
   def self.down
