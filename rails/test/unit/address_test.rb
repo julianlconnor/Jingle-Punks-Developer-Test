ff3de6929ba2e1bf2a08dcc4c136b53e90a5b589
Julian Connor <connoj3@rpi.edu>
2011-05-02 01:54:54 -0400

    Updated test

--- rails/test/unit/address_test.rb
+++ rails/test/unit/address_test.rb
@@ -1,17 +1,24 @@
 require 'test_helper'
 
 class AddressTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
+  # 5 digits, should pass
   test "Confirm that VALID records PASS validation and CAN be created (zip.size == 5)" do
     address = Address.new(:address_1 => "444 Park St", :city => "Montclair", :state => "nj", :zip => "07043", :user_id => 10)
     assert address.save
   end
+  # 7 digits, should fail
   test "Confirm that INVALID records FAIL validation CANNOT be created (zip.size > 5)" do
     address = Address.new(:address_1 => "444 Park St", :city => "Montclair", :state => "nj", :zip => "0704321", :user_id => 10)
     assert !address.save
   end
+  # 4 digits, should fail
   test "Confirm that INVALID records FAIL validation CANNOT be created (zip.size < 5)" do
     address = Address.new(:address_1 => "444 Park St", :city => "Montclair", :state => "nj", :zip => "0321", :user_id => 10)
+    assert !address.save
+  end
+  # non-digit, should fail
+  test "Confirm that INVALID records FAIL validation CANNOT be created (non digit)" do
+    address = Address.new(:address_1 => "444 Park St", :city => "Montclair", :state => "nj", :zip => "0321B", :user_id => 10)
     assert !address.save
   end
 end
